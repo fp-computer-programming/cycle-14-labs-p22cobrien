2decdbeaa77cdb435ed618a7e3f85837b16ae872
p22cobrien <p22cobrien@fairfieldprep.org>
2022-03-17 09:59:00 -0400

    submit lab 14

---
+++ lab_14-1.py
@@ -0,0 +1,20 @@
+# Author: CMOB 3/17/2022
+
+import turtle
+
+window = turtle.Screen()
+turtl = turtle.Turtle()
+
+window.setup(1000,1000)
+window.screensize(500,500)
+window.title("Lab 1")
+
+turtl.forward(250)
+turtl.right(90)
+turtl.forward(100)
+turtl.right(90)
+turtl.forward(250)
+turtl.right(90)
+turtl.forward(100)
+
+window.mainloop()
---
+++ lab_14-2.py
@@ -0,0 +1,19 @@
+# Author: CMOB 3/17/2022
+
+import turtle
+
+window = turtle.Screen()
+turtl = turtle.Turtle()
+
+window.setup(500,500)
+window.screensize(400,400)
+window.title("Lab 2")
+
+turtl.right(60)
+turtl.forward(100)
+turtl.right(120)
+turtl.forward(100)
+turtl.right(120)
+turtl.forward(100)
+
+window.mainloop()
